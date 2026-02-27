745f7fbec95e576fd829f1cbc269f990a3bd9cb8
Ron Ivan <rondaronivanr14@gmail.com>
2026-02-27 15:27:58 +0800

    Practice Lang

---
+++ resources/js/Pages/Upload.vue
@@ -0,0 +1,15 @@
+<div id="app"></div>
+
+<script src="https://unpkg.com/vue@3/dist/vue.global.js"></script>
+<script>
+const app = Vue.createApp({
+    template: `<h1>{{ message }}</h1>
+      <p>This is a second line of HTML code, inside back tick quotes</p>`,
+    data() {
+        return {
+            message: "Hello World!",
+        };
+    },
+});
+app.mount("#app");
+</script>
